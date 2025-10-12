05a93337252b5376f97f6957012bfb58a025a474
glaserdi <glaserdiuvegezo@gmail.com>
2025-10-12 18:47:12 +0300

    Update tarsas.py

--- tarsas.py
+++ tarsas.py
@@ -174,11 +174,11 @@ st.sidebar.title("🎲 Kérdőív")
 all_games=st.sidebar.checkbox("Összes játék mutatása")
 num_players = st.sidebar.slider("Hányan játszanátok?", 1, 10, 4)
 play_time = st.sidebar.selectbox("Mennyi időtök van?", ["15–30 perc", "30–60 perc", "60–120 perc", "2+ óra"])
-style_pref = st.sidebar.multiselect("Milyen típusú játékra vágytok?", ["party", "stratégiai", "kooperatív", "versengős", "gyerekbarát", "történetvezérelt", "logikai", "kártyajáték", "gyors"])
-complexity_pref = st.sidebar.radio("Milyen bonyolultságú legyen a játék?", ["egyszerű", "közepes", "összetett"])
+style_pref = st.sidebar.multiselect("Milyen típusú játékra vágytok?", ["party játék", "stratégiai", "kooperatív", "versengős", "gyerekbarát", "történetvezérelt", "logikai", "kártyajáték", "gyors"])
+complexity_pref = st.sidebar.radio("Milyen bonyolultságú legyen a játék?", ["nagyon egyszerű", "egyszerű", "közepesen összetett"])
 mechanism_pref = st.sidebar.multiselect(
     "Milyen mechanizmus tetszik?", 
-    ["pakliépítés", "kockadobás", "lapkalehelyezés", "blöffölés", "kooperáció", "draftolás", "szerepjáték", "programozás", "területfoglalás"]
+    ["pakliépítés", "kockadobás", "lapkalehelyezés", "blöff", "kooperáció", "draftolás", "szerepjáték", "programozás", "területfoglalás"]
 )
 only_one = st.sidebar.checkbox("Csak egy játékot ajálj")
 
@@ -241,6 +241,7 @@ if matched_games:
 
 else:
     st.warning("Sajnos nincs olyan játék, ami minden feltételnek megfelelne. Próbálj meg kevesebb szűrőt használni!")
+
 
 
 
