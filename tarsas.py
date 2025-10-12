98128dbff0287ee964c517402512e1e03e06c193
glaserdi <glaserdiuvegezo@gmail.com>
2025-10-12 18:42:25 +0300

    Update tarsas.py

--- tarsas.py
+++ tarsas.py
@@ -95,7 +95,7 @@ games = [
     {"name": "Spot It", "players": (2, 8), "duration": "15–30 perc", "complexity": "nagyon egyszerű", "style": ["kártyajáték", "party játék", "idő alapú", "gyerek játék"], "cooperative": False, "rules":"", "image": "games/spotit.png", "description": "A Spot it pörgős, zsebre vágható kártyajáték 5-féle játékváltozattal. A kártyákon 8-8 különböző képecske látható, és bármelyik két kártyán mindig van egy ugyanolyan belőlük. A játékosok feladata, hogy a többieknél gyorsabban megtalálják a közös képeket. A koncentrációt és a reflexeket fejlesztő társasjáték.", "mechanisms": ["aszimmetrikus", "kártya-képes", "kooperatív"],
      "author": "Denis Blanchot",
      "year": 2009},
-    {"name": "Magyar kártya", "players": (2, 16), "duration": "15–30 perc", "complexity": "nagyon egyszerű", "style": ["klasszikus", "versengős"], "cooperative": False, "rules":"", "image": "games/magyar.png", "description": "", "mechanisms": [kártya húzás],
+    {"name": "Magyar kártya", "players": (2, 16), "duration": "15–30 perc", "complexity": "nagyon egyszerű", "style": ["klasszikus", "versengős"], "cooperative": False, "rules":"", "image": "games/magyar.png", "description": "", "mechanisms": ["kártya húzás"],
 "author": "Johann Kaspar Frech",
 "year": 1830},
     {"name": "Ligretto (3 doboz)", "players": (2, 12), "duration": "15–30 perc", "complexity": "nagyon egyszerű", "style": ["idő alapú", "kártyajáték", "party játék"], "cooperative": False, "rules":"", "image": "games/ligretto.png", "description": "A Ligretto egy gyors kártyajáték, amely bármely korosztály számára remek szórakozás. Leülhet egy asztalhoz akár az egész család, senki sem fog unatkozni. A játékban a legfontosabb a gyorsaság. A lapokon lévő színek és számok egyaránt számítanak. A partit az kezdi, aki birtokolja az 1-es kártyalapot. Ezt követően minden játékos egyidejűleg próbálja meg lerakni a sorban következő és színben egyező lapjait. A gyorsaság dönt, ki teheti le végül a kártyát, a keresést azonban megnehezíti, hogy a kártyákat több helyen kell tartani, és sok esetben nem is lehet megnézni minden lapot. A végén pontozással dől el, ki a nyertes. A szabályok könnyen elsajátíthatók. A játék gyors, koncentrációt igényel, de közben szórakoztató és rendkívül izgalmas.", "mechanisms": ["minták", "valósidejű", "gyorsaság"],
@@ -241,6 +241,7 @@ if matched_games:
 
 else:
     st.warning("Sajnos nincs olyan játék, ami minden feltételnek megfelelne. Próbálj meg kevesebb szűrőt használni!")
+
 
 
 
